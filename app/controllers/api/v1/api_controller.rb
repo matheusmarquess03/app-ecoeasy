f7674a9b79ba8ce8ee75b9b9be6b169fb8355449
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-08-20 15:32:03 -0300

    Create endpoint to Truckers Login on API

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,10 @@
 module Api::V1
-  class ApiController < ApplicationController
+  class ApiController <  ActionController::API
+    include DeviseTokenAuth::Concerns::SetUserByToken
+
+    skip_before_action :verify_authenticity_token
+    before_action :authenticate_user!
+
     # Métodos globais
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
+  ## Routes to Users - using Devise Token Auth
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+    end
+  end
+
+  ## Routes to Admins
+  # Devise redirects configurations
+  devise_for :admins, controllers: { sessions: 'devise/admins/sessions' }
+
+  # Routes to Backoffice portal (admin)
   namespace :backoffice do
     get '', to: 'dashboard#index'
 
     resources :truckers
   end
-
-  devise_for :admins, controllers: { sessions: 'devise/admins/sessions' }
-  devise_for :users
 
 end
