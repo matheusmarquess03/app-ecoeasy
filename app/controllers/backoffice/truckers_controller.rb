71f3740f7edd4cea975567104ed0bab96854a49e
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-08-21 01:20:06 -0300

    Adjust Trcker Controller to use User Model

--- app/controllers/backoffice/truckers_controller.rb
+++ app/controllers/backoffice/truckers_controller.rb
@@ -1,32 +1,33 @@
 class Backoffice::TruckersController < BackofficeController
+  before_action :set_trucker_default_data, only: [:create]
+
   def index
-    @truckers = Trucker.all
+    @truckers = User.trucker
   end
 
   def new
-    @trucker = Trucker.new
+    @trucker = User.new
   end
 
   def create
-    @trucker = Trucker.new(trucker_params)
-    if @trucker.save
+    if @trucker.save!
       flash[:success] = 'Caminhoneiro cadastrado com sucesso'
     else
-      flash[:success] = 'Falha para cadastrar o Caminhoneiro. Tente novamente mais tarde'
+      flash[:alert] = 'Falha para cadastrar o Caminhoneiro. Tente novamente mais tarde'
     end
     redirect_to backoffice_truckers_path
   end
 
   def edit
-    @trucker = Trucker.find(params[:id])
+    @trucker = User.find(params[:id])
   end
 
   def update
-    @trucker = Trucker.find(params[:id])
-    if @trucker.update(trucker_params)
+    @trucker = User.find(params[:id])
+    if @trucker.update!(trucker_params)
       flash[:success] = 'Caminhoneiro cadastrado com sucesso'
     else
-      flash[:success] = 'Falha para cadastrar o Caminhoneiro. Tente novamente mais tarde'
+      flash[:alert] = 'Falha para cadastrar o Caminhoneiro. Tente novamente mais tarde'
     end
     redirect_to backoffice_truckers_path
   end
@@ -37,6 +38,18 @@ class Backoffice::TruckersController < BackofficeController
   private
 
   def trucker_params
-    params.fetch(:trucker, {}).permit(:name, :email)
+    return params.fetch(:user, {}).permit(:name, :email) if password_blank?
+    params.fetch(:user, {}).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def password_blank?
+    params[:user][:password].blank? &&
+    params[:user][:password_confirmation].blank?
+  end
+
+  def set_trucker_default_data
+    @trucker = User.new(trucker_params)
+    @trucker.access_profile = 'trucker'
+    @trucker.provider = 'email'
   end
 end
