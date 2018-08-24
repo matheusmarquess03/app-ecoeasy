4e099f328f8201da95d5127fbc8646e0b5536b3e
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-08-24 20:48:19 -0300

    Use Trucker Class in trucker_controller

--- app/controllers/backoffice/truckers_controller.rb
+++ app/controllers/backoffice/truckers_controller.rb
@@ -2,11 +2,11 @@ class Backoffice::TruckersController < BackofficeController
   before_action :set_trucker_default_data, only: [:create]
 
   def index
-    @truckers = User.trucker
+    @truckers = Trucker.all
   end
 
   def new
-    @trucker = User.new
+    @trucker = Trucker.new
   end
 
   def create
@@ -19,11 +19,11 @@ class Backoffice::TruckersController < BackofficeController
   end
 
   def edit
-    @trucker = User.find(params[:id])
+    @trucker = Trucker.find(params[:id])
   end
 
   def update
-    @trucker = User.find(params[:id])
+    @trucker = Trucker.find(params[:id])
     if @trucker.update!(trucker_params)
       flash[:success] = 'Caminhoneiro cadastrado com sucesso'
     else
@@ -38,18 +38,17 @@ class Backoffice::TruckersController < BackofficeController
   private
 
   def trucker_params
-    return params.fetch(:user, {}).permit(:name, :email) if password_blank?
-    params.fetch(:user, {}).permit(:name, :email, :password, :password_confirmation)
+    return params.fetch(:trucker, {}).permit(:name, :email) if password_blank?
+    params.fetch(:trucker, {}).permit(:name, :email, :password, :password_confirmation)
   end
 
   def password_blank?
-    params[:user][:password].blank? &&
-    params[:user][:password_confirmation].blank?
+    params[:trucker][:password].blank? &&
+    params[:trucker][:password_confirmation].blank?
   end
 
   def set_trucker_default_data
-    @trucker = User.new(trucker_params)
-    @trucker.access_profile = 'trucker'
+    @trucker = Trucker.new(trucker_params)
     @trucker.provider = 'email'
   end
 end
