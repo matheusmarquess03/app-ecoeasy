2e98f8747f07ff556fe6d0dee5eb1438d6278fe1
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-09-24 13:42:58 -0300

    Fix heroku error 4

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,10 +1,8 @@
-module Api::V1
-  class ApiController <  ActionController::API
-    include DeviseTokenAuth::Concerns::SetUserByToken
+class Api::V1::ApiController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
 
-    # skip_before_action :verify_authenticity_token
-    before_action :authenticate_user!
+  # skip_before_action :verify_authenticity_token
+  before_action :authenticate_user!
 
     # Métodos globais
-  end
 end
