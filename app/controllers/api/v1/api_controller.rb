62e3af06949796931f419af1877bbb326a68602c
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-09-24 12:08:20 -0300

    Fix heroku error 3

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,7 +2,7 @@ module Api::V1
   class ApiController <  ActionController::API
     include DeviseTokenAuth::Concerns::SetUserByToken
 
-    skip_before_action :verify_authenticity_token
+    # skip_before_action :verify_authenticity_token
     before_action :authenticate_user!
 
     # Métodos globais
