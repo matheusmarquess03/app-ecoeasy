f6dbc924d8534decd60962b292140026026f7c2e
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-10-21 13:52:58 -0300

    Include Address when create client

--- app/controllers/api/v1/devise/registrations_controller.rb
+++ app/controllers/api/v1/devise/registrations_controller.rb
@@ -3,14 +3,85 @@ module Api::V1::Devise
     skip_before_action :verify_authenticity_token
     before_action :configure_permitted_parameters
 
+    def create
+      build_resource
+
+      unless @resource.present?
+        raise DeviseTokenAuth::Errors::NoResourceDefinedError,
+              "#{self.class.name} #build_resource does not define @resource,"\
+              ' execution stopped.'
+      end
+
+      # give redirect value from params priority
+      @redirect_url = params.fetch(
+        :confirm_success_url,
+        DeviseTokenAuth.default_confirm_success_url
+      )
+
+      # success redirect url is required
+      if confirmable_enabled? && !@redirect_url
+        return render_create_error_missing_confirm_success_url
+      end
+
+      # if whitelist is set, validate redirect_url against whitelist
+      return render_create_error_redirect_url_not_allowed if blacklisted_redirect_url?
+
+      begin
+        # override email confirmation, must be sent manually from ctrl
+        resource_class.set_callback('create', :after, :send_on_create_confirmation_instructions)
+        resource_class.skip_callback('create', :after, :send_on_create_confirmation_instructions)
+
+        if @resource.respond_to? :skip_confirmation_notification!
+          # Fix duplicate e-mails by disabling Devise confirmation e-mail
+          @resource.skip_confirmation_notification!
+        end
+
+        if @resource.save
+          yield @resource if block_given?
+
+          if @resource.confirmed?
+            # email auth has been bypassed, authenticate user
+            @client_id, @token = @resource.create_token
+            @resource.save!
+            update_auth_header
+          else
+            # user will require email authentication
+            @resource.send_confirmation_instructions(
+              client_config: params[:config_name],
+              redirect_url: @redirect_url
+            )
+          end
+
+          create_address if address_params.present?
+          render_create_success
+        else
+          clean_up_passwords @resource
+          render_create_error
+        end
+      rescue ActiveRecord::RecordNotUnique
+        clean_up_passwords @resource
+        render_create_error_email_already_exists
+      end
+    end
+
+    private
+
+    def create_address
+      address = Address.new(address_params)
+      address.user = @resource
+      address.save
+    end
+
+    def address_params
+      params.fetch(:address, {}).permit(:street, :number, :complement, :district, :city, :state, :country, :zip_code)
+    end
+
     protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(
-          :sign_up,
-          keys:[
-            :email, :password, :password_confirmation, :name, :cpf, :phone_number
-          ]
+        :sign_up,
+        keys: [ :email, :password, :name, :cpf, :phone_number ]
       )
     end
   end
