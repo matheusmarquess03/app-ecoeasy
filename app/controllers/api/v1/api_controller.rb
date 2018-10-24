4dfadaa3e1b8dd12565fac388e2c14b64ebe598c
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-10-24 01:49:34 -0300

    Fix: call helper authenticate_api_v1_user before all request api

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -2,6 +2,6 @@ module Api::V1
   class ApiController < ActionController::API
     include DeviseTokenAuth::Concerns::SetUserByToken
 
-    before_action :authenticate_user!
+    before_action :authenticate_api_v1_user!
   end
 end
