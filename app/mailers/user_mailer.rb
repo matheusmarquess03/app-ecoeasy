c722c9808e3f263aaed3855c81b6c13347508e0b
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-11-18 03:06:53 -0200

    Fix 2: Include bcc to recovery email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,10 +5,5 @@ class UserMailer < ApplicationMailer
     mail to: @user.email,
          bcc: ['a.hugofonseca@gmail.com', 'david.faculdade@gmail.com', 'gustavovgarcia.sl@gmail.com'],
          subject: 'Senha temporária'
-
-    mail to: @voucher.recipient.email,
-         cc: cc << ENV['EASYLIVE_NOREPLY_EMAIL'],
-         bcc: @bcc,
-         subject: "EASYLIVE - Confirmação de resgate"
   end
 end
