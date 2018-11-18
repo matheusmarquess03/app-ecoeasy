6380ab234b8758bbfc5fa22e8031d59bacf9666f
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-11-18 02:50:56 -0200

    Include more configurations to Gmail SMTP 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name: ENV["GMAIL_USERNAME"],
-    password: ENV["GMAIL_PASSWORD"],
+    user_name:            'ecoeasy.contato@gmail.com',
+    password:             '1234Trocar',
     authentication:       'plain',
     enable_starttls_auto: true
   }
