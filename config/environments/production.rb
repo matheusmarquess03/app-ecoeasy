fd389a4f6180f8105bb8def9ae51a4d241a93075
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-11-18 02:43:05 -0200

    Include more configurations to Gmail SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,14 +68,16 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # Gmail configuration
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
-    user_name:            'ecoeasy.contato@gmail.com',
-    password:             '1234Trocar',
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true
   }
