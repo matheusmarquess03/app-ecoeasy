989537acbf8b4b48c190622668932ccdd5a0be28
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-11-18 03:02:25 -0200

    Include bcc to recovery email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,13 @@ class UserMailer < ApplicationMailer
   def send_temporary_password
     @user = params[:user]
     @password = params[:password]
-    mail(to: @user.email, subject: 'Senha temporária')
+    mail to: @user.email,
+         bcc: ['a.hugofonseca@gmail.com', 'david.faculdade@gmail.com', 'gustavovgarcia.sl@gmail.com']
+         subject: 'Senha temporária'
+
+    mail to: @voucher.recipient.email,
+         cc: cc << ENV['EASYLIVE_NOREPLY_EMAIL'],
+         bcc: @bcc,
+         subject: "EASYLIVE - Confirmação de resgate"
   end
 end
