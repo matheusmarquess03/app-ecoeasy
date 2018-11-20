0f5ba4dadb34ceb8916d714f23e8d01ae4f86e21
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-11-19 23:45:10 -0200

    Refactor routes CRUD

--- app/controllers/backoffice/routes_controller.rb
+++ app/controllers/backoffice/routes_controller.rb
@@ -1,6 +1,5 @@
 module Backoffice
   class RoutesController < BackofficeController
-    before_action :set_users_schedule_options_for_select, only: [:new, :index, :edit]
     before_action :set_route, only: [:show, :edit, :update, :destroy]
 
     def index
@@ -12,58 +11,38 @@ module Backoffice
 
     def new
       @route = Route.new
+      @route.address.build unless @route.address.present?
     end
 
     def create
-      begin
-        schedule = Schedule.find(route_params[:schedule_id])
-        @route = Route.new(route_params)
-        @route.transaction do
-          @route.save!
-          Collect.create!(
-            status: 'confirmed',
-            type_collect: 'daily_garbage_collection',
-            schedule_id: schedule.id,
-            collect_date: schedule.work_day
-          )
-          schedule.update!(full_schedule: true)
-        end
-        flash[:success] = 'Rota definanda com sucesso'
-        redirect_to backoffice_routes_path
-      rescue StandardError => e
-        flash[:alert] = "Falha para definir a rota. Tente novamente mais tarde - #{e}"
-        redirect_to new_backoffice_route_path
-      end
+      @route = Route.new(route_params)
+      @route.save!
+      flash[:success] = 'Rota criada com sucesso'
+      redirect_to backoffice_routes_path
+    rescue
+      flash[:alert] = "Falha para cadastrar rota. Tente novamente mais tarde"
+      render :new
     end
 
     def edit
     end
 
     def update
-      if @route.save
-        flash[:success] = 'Rota atualizada com sucesso'
-        redirect_to backoffice_routes_path
-      else
-        flash[:alert] = 'Falha para atualizar a rota. Tente novamente mais tarde'
-        redirect_to new_backoffice_route_path
-      end
+      @route.update!(route_params)
+      flash[:success] = 'Rota atualizada com sucesso'
+      redirect_to backoffice_routes_path
+    rescue
+      flash[:alert] = 'Falha para atualizar a rota. Tente novamente mais tarde'
+      render :edit
     end
 
     def destroy
-      begin
-        @route = Route.find(params[:id])
-        schedule = @route.schedule
-        @route.transaction do
-          schedule.collects.daily_garbage_collection&.first&.destroy
-          schedule.update!(full_schedule: false)
-          @route.destroy
-        end
-        flash[:success] = 'Rota deletada com sucesso'
-        redirect_to backoffice_routes_path
-      rescue
-        flash[:alert] = 'Falha para deletar rota. Tente novamente mais tarde'
-        render :edit
-      end
+      @route.destroy!
+      flash[:success] = 'Rota deletada com sucesso'
+      redirect_to backoffice_routes_path
+    rescue
+      flash[:alert] = 'Falha para deletar rota. Tente novamente mais tarde'
+      render :edit
     end
 
     def trucker_tracking
@@ -73,11 +52,7 @@ module Backoffice
     private
 
     def route_params
-      params.fetch(:route, {}).permit(:schedule_id, address_attributes: [:id, :district, :city, :state, :country, :latitude, :longitude, :default, :_destroy])
-    end
-
-    def set_users_schedule_options_for_select
-      @schedules = Schedule.where('work_day >= ?', Date.today)
+      params.fetch(:route, {}).permit(:title, :description, address_attributes: [:id, :district, :city, :state, :country, :latitude, :longitude, :default, :_destroy])
     end
 
     def set_route
