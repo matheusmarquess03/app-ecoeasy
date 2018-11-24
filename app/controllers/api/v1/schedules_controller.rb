67d1970ac0435366357148b087bdf0db6a5761c8
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-11-24 00:34:52 -0200

    Remove endpoint wrong in Schedule

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -12,27 +12,7 @@ module Api::V1
       render json: { message: e.message }, status: 500
     end
 
-    def update
-      unless @schedule.present?
-        render json: { message: 'Não há coletas agendadas para hoje para este caminhoneiro' }
-      end
-      @schedule.collects.map do |c|
-        c.transaction do
-          c.collected!
-          c.update!(schedule_params)
-        end
-      end
-    rescue ActiveRecord::RecordInvalid => e
-      render json: { message: e.message }, status: 422
-    rescue StandardError => e
-      render json: { message: e.message }, status: 500
-    end
-
     private
-
-    def schedule_params
-      params.permit(:landfill_id)
-    end
 
     def set_day_schedule
       @schedule = Schedule.where(user_id: current_api_v1_user.id, work_day: Date.today).first
