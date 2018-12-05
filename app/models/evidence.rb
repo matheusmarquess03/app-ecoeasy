9a21fe9ff6567de6d3db414696fd927a2bad9982
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-12-05 01:55:00 -0200

    Configuration evidence_type enum

--- app/models/evidence.rb
+++ app/models/evidence.rb
@@ -1,9 +1,14 @@
 class Evidence < ApplicationRecord
+  # Enumerators
+  enum evidence_type: [:simple_evidence, :incident]
+
+  # Associations
   belongs_to :user
   belongs_to :address, optional: true
 
   has_many_attached :images
 
+  # Methods
   def supervisor
     self.user
   end
