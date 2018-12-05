cd23cde0cbb32bc3f3ed8af484a8f4d98820dc3c
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2018-12-05 01:38:44 -0200

    Include image file in Evidence Endpoint

--- app/controllers/api/v1/evidences_controller.rb
+++ app/controllers/api/v1/evidences_controller.rb
@@ -3,6 +3,7 @@ module Api::V1
     def create
       @evidence = Evidence.new(evidence_params)
       @evidence.user_id = current_api_v1_user.id
+      convert_base64_to_image_file
       @evidence.save!
     rescue ActiveRecord::RecordInvalid => e
       render json: { message: e.message }, status: 422
@@ -14,6 +15,23 @@ module Api::V1
 
     def evidence_params
       params.fetch(:evidence, {}).permit(:description)
+    end
+
+    def convert_base64_to_image_file
+      # Process the file, decode the base64 encoded file
+      @decoded_file = Base64.decode64(params[:images])
+
+      @filename = "image_file_#{Time.now.to_i}"     # this will be used to create a tmpfile and also, while setting the filename to attachment
+      @tmp_file = Tempfile.new([@filename, '.png']) # This creates an in-memory file [details here][1]
+      @tmp_file.binmode                             # This helps writing the file in binary mode.
+      @tmp_file.write @decoded_file
+      @tmp_file.rewind()
+
+      # We create a new model instance
+      @evidence.images.attach(io: @tmp_file, filename: @filename) # attach the created in-memory file, using the filename defined
+
+      # deletes the temp file
+      @tmp_file.unlink
     end
   end
 end
