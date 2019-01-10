ed24330872a5cd1e592f3ae3b6164aec067939b3
Lucas Mansur <lucas.mansur2@gmail.com>
2019-01-09 22:50:34 -0200

    Redirect to Show after updating Contract

--- app/controllers/backoffice/contracts_controller.rb
+++ app/controllers/backoffice/contracts_controller.rb
@@ -2,11 +2,13 @@
 
 module Backoffice
   class ContractsController < BackofficeController
-    before_action :set_contract, only: [:edit, :update, :destroy]
+    before_action :set_contract, only: %i[show edit update destroy]
 
     def index
       @contracts = Contract.all
     end
+
+    def show; end
 
     def new
       @contract = Contract.new
@@ -26,10 +28,9 @@ module Backoffice
     def edit; end
 
     def update
-      @contract.images.attach(contract_params[:images])
       @contract.update!(contract_params)
       flash[:success] = 'Contrato atualizado com sucesso'
-      redirect_to backoffice_contracts_path
+      redirect_to [:backoffice, @contract]
     rescue StandardError
       flash[:alert] = 'Falha ao atualizar o contrato.'
       render :edit
@@ -38,17 +39,17 @@ module Backoffice
     def destroy
       if @contract.destroy
         flash[:success] = 'Contrato deletado com sucesso'
-        redirect_to backoffice_contracts_path
       else
         flash[:alert] = 'Falha ao deletar o contrato.'
-        redirect_to backoffice_contracts_path
       end
+
+      redirect_to backoffice_contracts_path
     end
 
     private
 
     def contract_params
-      params.fetch(:contract, {}).permit(:name, :observation, images: [])
+      params.fetch(:contract, {}).permit(:name, :observation, attachments: [])
     end
 
     def set_contract
