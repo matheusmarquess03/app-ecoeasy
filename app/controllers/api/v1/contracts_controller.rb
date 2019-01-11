aff3ef7af75244debf6f88cef8c9fb90d3d0ecf4
Lucas Mansur <lucas.mansur2@gmail.com>
2019-01-10 23:03:23 -0200

    Add Create/Update/Delete to Contracts API

--- app/controllers/api/v1/contracts_controller.rb
+++ app/controllers/api/v1/contracts_controller.rb
@@ -1,5 +1,7 @@
 module Api::V1
   class ContractsController < ApiController
+    before_action :set_contract, only: %i[update destroy]
+
     def index
       @contracts = Contract.all
     rescue ActiveRecord::RecordInvalid => e
@@ -14,6 +16,41 @@ module Api::V1
       render json: { message: e.message }, status: 422
     rescue StandardError => e
       render json: { message: e.message }, status: 500
+    end
+
+    def create
+      @contract = Contract.new(contract_params)
+      @contract.save!
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { message: e.message }, status: 422
+    rescue StandardError => e
+      render json: { message: e.message }, status: 500
+    end
+
+    def update
+      @contract.update!(contract_params)
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { message: e.message }, status: 422
+    rescue StandardError => e
+      render json: { message: e.message }, status: 500
+    end
+
+    def destroy
+      @contract.destroy!
+    rescue ActiveRecord::RecordInvalid => e
+      render json: { message: e.message }, status: 422
+    rescue StandardError => e
+      render json: { message: e.message }, status: 500
+    end
+
+    private
+
+    def contract_params
+      params.fetch(:contract, {}).permit(:name, :observation, attachments: [])
+    end
+
+    def set_contract
+      @contract = Contract.find(params[:id])
     end
   end
 end
