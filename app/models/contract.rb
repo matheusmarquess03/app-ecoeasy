7f6de97462afc173817d9e665f93af60f03ca452
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2019-01-18 22:50:04 -0200

    Fix

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -2,4 +2,8 @@
 
 class Contract < ApplicationRecord
   has_many_attached :attachments
+
+  def get_path_attachments
+    ActiveStorage::Blob.service.send(:path_for, attachments.first.key)
+  end
 end
