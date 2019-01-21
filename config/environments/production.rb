f2b4259b8a57c8ff2b5d4eff344048c556bf8f5e
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2019-01-21 16:06:39 -0200

    Adjust send mail 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,23 +73,10 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'ecoeasyapp.com.br',
+    :domain         => 'ecoeasy-ro.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
-
-
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.smtp_settings = {
-  #   :user_name => ENV['SENDGRID_USERNAME'],
-  #   :password => ENV['SENDGRID_PASSWORD'],
-  #   :domain => 'ecoeasyapp.com.br',
-  #   :address => 'smtp.sendgrid.net',
-  #   :port => 587,
-  #   :authentication => :plain,
-  #   :enable_starttls_auto => true
-  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
