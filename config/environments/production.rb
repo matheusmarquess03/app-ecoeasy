b34d76ee4a4b9c6adccd2358f874622cfd7e7c6a
Hugo Fonseca <hugo.fonseca@fulllab.com.br>
2019-01-21 13:07:44 -0200

    Configure mailgun to send mail in project

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,35 +67,29 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # Gmail configuration
-  # config.action_mailer.default_url_options = { :host => 'http://www.ecoeasyapp.com.br', port: 80 }
-  # config.action_mailer.default :charset => "utf-8"
-  # config.action_mailer.perform_deliveries = true
-  # config.action_mailer.raise_delivery_errors = false
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.sendgrid.net',
-  #   port:                 587,
-  #   domain:               'ecoeasyapp.com.br',
-  #   user_name:            ENV['SENDGRID_USERNAME'],
-  #   password:             ENV['SENDGRID_PASSWORD'],
-  #   authentication:       'plain',
-  #   enable_starttls_auto: true
-  # }
-
   config.action_mailer.default_url_options = { host: 'ecoeasyapp.com.br', port: 80 }
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'ecoeasyapp.com.br',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'ecoeasyapp.com.br',
     :authentication => :plain,
-    :enable_starttls_auto => true
   }
+  config.action_mailer.delivery_method = :smtp
+
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.smtp_settings = {
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD'],
+  #   :domain => 'ecoeasyapp.com.br',
+  #   :address => 'smtp.sendgrid.net',
+  #   :port => 587,
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true
+  # }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
